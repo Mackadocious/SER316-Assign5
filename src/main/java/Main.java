2669ba369857c1f3d015231b0c80bed22131dcc6
Alexander Mack <ajmack5@asu.edu>
2020-04-21 10:59:40 -0700

    adds tests, sets up jacoco and spotbugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 package main.java;
 
 public class Main {
-    public static void main(String[] args){
+    /**
+     * main method.
+     * @summary main method.
+     * @param args input args
+     */
+    public static void main(String[] args) {
 
         System.out.println("Hello World");
+        String temp = hello();
+        System.out.println(temp);
+    }
+
+    /**
+     * method that returns hello.
+     * @summary method that returns hello
+     * @return returns hello
+     */
+    public static java.lang.String hello() {
+        return "hello";
     }
 }
